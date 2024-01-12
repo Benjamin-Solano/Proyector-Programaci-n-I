d15472f92f3db9557b959cbffeada769e12d1a10
Benjamin Solano <benjaminalexander958@gmail.com>
2024-01-12 15:56:33 -0600

    se ha recuperado el main

--- Proyecto/Proyecto/main.cpp
+++ Proyecto/Proyecto/main.cpp
@@ -3,7 +3,28 @@
 int main() {
 	system("color 03");
 
-	cout << "Nuevo comentario" << endl;
+	system("color 03");
+
+
+	Mazo* mazo = new Mazo();
+	mazo->Inicializar();
+	mazo->barajar();
+
+	Mano* mano = new Mano();
+	JugadorGenerico* player = new JugadorGenerico("BENJAMIN", mano);
+	mano->agregarCarta(mazo);
+	mano->agregarCarta(mazo);
+
+
+	mano->getPuntos();
+
+	cout << player->toString() << endl;
+
+	// CONSULTAR CON EL PROFESOR ESTE METODO...
+	cout << player->pedirCarta(mazo)->toString() << endl;
+
+
+	system("pause");
 
 
 	system("pause");
