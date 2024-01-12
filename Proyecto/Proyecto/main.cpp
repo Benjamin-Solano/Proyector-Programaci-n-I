53bb9e1df9e97246280d9249485d0f4cdc369327
Benjamin Solano <benjaminalexander958@gmail.com>
2024-01-12 15:50:24 -0600

    Cambios en main

--- Proyecto/Proyecto/main.cpp
+++ Proyecto/Proyecto/main.cpp
@@ -3,23 +3,7 @@
 int main() {
 	system("color 03");
 
-	
-	Mazo* mazo = new Mazo();
-	mazo->Inicializar();
-	mazo->barajar();
-	
-	Mano* mano = new Mano();
-	JugadorGenerico* player = new JugadorGenerico("BENJAMIN", mano);
-	mano->agregarCarta(mazo);
-	mano->agregarCarta(mazo);
-	
-		
-	mano->getPuntos();
-	
-	cout << player->toString() << endl;
-
-	// CONSULTAR CON EL PROFESOR ESTE METODO...
-	cout << player->pedirCarta(mazo)->toString() << endl;
+	cout << "Nuevo comentario" << endl;
 
 
 	system("pause");
